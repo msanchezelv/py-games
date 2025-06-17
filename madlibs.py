5d45f1306f636ac84dd97b6a335f3d13ac1e631a
msanchezelv <msanchezelv@uoc.edu>
2025-06-17 13:46:33 +0200

    Madlibs commit

---
+++ madlibs.py
@@ -0,0 +1,12 @@
+noun1 = input("Write a noun: ")
+noun2 = input("Write another noun: ")
+noun3 = input("Write another noun: ")
+verb1 = input("Write a verb: ")
+verb2 = input("Write another verb: ")
+verb3 = input("Write another verb: ")
+adjective1 = input("Write an adjective: ")
+
+
+madlibs = f"Hey! Welcome to my {noun1}. This {noun1}  consists in the user  {verb1}ing  {noun2} to {verb2} a {noun3}  that will {verb3} completely {adjective1}. Have fun!"
+
+print(madlibs)
